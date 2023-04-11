68ba69e243bd918061982c7cf9d71a7be853c438
ObuchiYuki <yukibochi1@gmail.com>
2023-04-11 19:29:33 +0900

    add LZObservableProperty

---
+++ Sources/lib0/LZObservableProperty.swift
@@ -0,0 +1,25 @@
+//
+//  Property.swift
+//  CoreUtil
+//
+//  Created by yuki on 2020/07/06.
+//  Copyright © 2020 yuki. All rights reserved.
+//
+
+import Combine
+
+// from CoreUtil (https://github.com/ObuchiYuki/CoreUtil) 's Observable
+@propertyWrapper
+public struct LZObservableProperty<Value> {
+    @inlinable public var projectedValue: some Publisher<Value, Never> { subject }
+    
+    @usableFromInline let subject: CurrentValueSubject<Value, Never>
+    
+    @inlinable public var wrappedValue: Value {
+        @inlinable get { subject.value }
+        @inlinable set { subject.send(newValue) }
+    }
+    @inlinable public init(wrappedValue value: Value) {
+        self.subject = CurrentValueSubject(value)
+    }
+}
